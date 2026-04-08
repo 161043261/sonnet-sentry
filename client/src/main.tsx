d4e5fab9b8b794063bc1b19e64ee95752b9093aa
tianchenghang <tiancheng.hang@bytedance.com>
2026-04-09 02:57:44 +0800

    feat: Introduce new features

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-import { init, sonnetEnable } from "@sonnet-sentry/core";
-import PerformancePlugin from "@sonnet-sentry/performance";
-import ScreenRecordPlugin from "@sonnet-sentry/screen-record";
+import { init, sonnetEnable } from "sonnet-sentry";
+import PerformancePlugin from "sonnet-sentry/plugins/perf";
+import ScreenRecordPlugin from "sonnet-sentry/plugins/record";
 
 init({ dsn: "/api/log" });
 sonnetEnable(PerformancePlugin);
